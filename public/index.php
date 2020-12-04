dda292fe8bcb6be6fc59eefe8163486962ed7a71
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2020-12-04 22:10:16 +0630

    Added View Render Method And BaseController

--- public/index.php
+++ public/index.php
@@ -1,13 +1,14 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 use app\core\Application;
-
+use app\controllers\SiteController;
 
 $app = new Application();
 $app->router->get('/','home');
 
-$app->router->get('/contact',function (){
-  return 'I  am contact';
-});
+$app->router->get('/contact',[SiteController::class,'contact']);
+$app->router->post('/contact','contact');
 
 $app->run();
+
+?>
